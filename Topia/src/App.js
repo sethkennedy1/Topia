ba7b032be6c4b927575275ca1b304ab15648e53d
Seth Kennedy <sethkennedy01@Seths-MacBook-Pro.local>
2024-10-05 01:05:12 -0700

    idk

---
+++ Topia/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://via.placeholder.com/150" className="App-logo" alt="logo" />
+      </header>
+      <main className="App-main">
+        <h1>Welcome to Our Home Page</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ build/precache-manifest.e7097ff551a03b2d2fdf36b379a972ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c78f362e949da1a85047027d3a0572",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1160627cfd0707e340ee",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "1160627cfd0707e340ee",
+    "url": "/static/js/main.b25acd25.chunk.js"
+  },
+  {
+    "revision": "60bc7f864dddbc003152",
+    "url": "/static/js/runtime~main.71a348c3.js"
+  }
+]);
